9d63f1cfe2b4cd7dd681911c88b658ec867bb1c8
UPIN-0583 <quochuystg2004@gmail.com>
2025-06-10 14:54:49 +0700

    Sửa phần navbar cố định

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -124,7 +124,7 @@ const LogNavbar = () => {
 
 const GuestNavbar = () => {
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-white shadow-md fixed top-0 left-0 right-0 z-50">
       {/* Top Bar */}
       <div className="bg-purple-700 text-white text-sm text-center py-2 flex flex-col md:flex-row justify-between px-4 md:px-20">
         <span>Hotline: +123-456-789</span>
